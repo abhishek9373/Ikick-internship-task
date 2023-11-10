e312f97cb93d326b25f2e03d8584842cd8deaf07
= <=>
2023-11-10 23:06:16 +0530

    created required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ListingPageComponent } from './components/listing-page/listing-page.component';
+import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ListingPageComponent,
+    RightSidebarComponent,
   ],
   imports: [
     BrowserModule
